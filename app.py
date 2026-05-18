d911b3438cc89961f19334e1d1042b5f0b465ee0
ankeshk554-bit <ankeshk554@gmail.com>
2026-05-18 23:45:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,7 @@
-import streamlit as st
-import pandas as pd
-
-from core.utils import load_data
-from core.indicators import compute_indicators
-from core.market_regime import get_market_regime
-from core.scanner import run_universe_scan
-from core.charts import institutional_chart
-from core.backtest import run_backtest
-
 from data.nifty50 import NIFTY50
 from data.nifty200 import NIFTY200
 from data.nifty500 import NIFTY500
-import streamlit as st
+
 from ui.layout import screener_tab, backtest_tab, quant_lab_tab
 from core.screener import run_universe_scan   # ✅ corrected import
 
